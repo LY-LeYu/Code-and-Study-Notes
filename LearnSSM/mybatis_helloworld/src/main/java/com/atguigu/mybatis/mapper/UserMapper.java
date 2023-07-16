c51a549ee0f9440e48d2d4eb5c2b209950d7cb7c
Losrik <ly_leyu@outlook.com>
2023-07-16 20:16:48 +0800

    单一使用MyBatis框架测试数据库的增删改查

--- LearnSSM/mybatis_helloworld/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
+++ LearnSSM/mybatis_helloworld/src/main/java/com/atguigu/mybatis/mapper/UserMapper.java
@@ -1,5 +1,17 @@
 package com.atguigu.mybatis.mapper;
 
+import com.atguigu.mybatis.pojo.User;
+
+import java.util.List;
+
 public interface UserMapper {
     int insertUser();
+    int deleteUser();
+    int updateUser();
+
+    User selectUser();
+
+    List<User> selectAllUser();
+
+
 }
--- LearnSSM/mybatis_helloworld/src/test/java/com/atguigu/mybatis/Test1.java
+++ LearnSSM/mybatis_helloworld/src/test/java/com/atguigu/mybatis/Test1.java
@@ -1,6 +1,8 @@
 package com.atguigu.mybatis;
 
 import com.atguigu.mybatis.mapper.UserMapper;
+import com.atguigu.mybatis.pojo.User;
+import com.atguigu.mybatis.utils.SqlSessionUtil;
 import org.apache.ibatis.io.Resources;
 import org.apache.ibatis.session.SqlSession;
 import org.apache.ibatis.session.SqlSessionFactory;
@@ -9,10 +11,11 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.List;
 
 public class Test1 {
     @Test
-    public void testInsert() throws IOException {
+    public void testInsertUser() throws IOException {
         //获取核心文件输入流
         InputStream resourceAsStream = Resources.getResourceAsStream("mybatis-config.xml");
 
@@ -26,9 +29,43 @@ public class Test1 {
 
         int result = mapper.insertUser();
 
+//        int result = sqlSession.insert("com.atguigu.mybatis.mapper.UserMapper.insertUser");
+
         System.out.println(result);
         sqlSession.commit();
         sqlSession.close();
 
+    }
+
+    @Test
+    public void testUpdateUser(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapperUpdate = sqlSession.getMapper(UserMapper.class);
+        int result = mapperUpdate.updateUser();
+    }
+
+    @Test
+    public void testDeleteUser(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapperDelete = sqlSession.getMapper(UserMapper.class);
+        int result = mapperDelete.deleteUser();
+    }
+
+    @Test
+    public void testSelectUser(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapperSelect = sqlSession.getMapper(UserMapper.class);
+        User x1 = mapperSelect.selectUser();
+        System.out.println(x1);
+    }
+
+    @Test
+    public void testSelectAllUser(){
+        SqlSession sqlSession = SqlSessionUtil.getSqlSession();
+        UserMapper mapperSelectAll = sqlSession.getMapper(UserMapper.class);
+        List x1 = mapperSelectAll.selectAllUser();
+        for (Object o : x1) {
+            System.out.println(o);
+        }
     }
 }
