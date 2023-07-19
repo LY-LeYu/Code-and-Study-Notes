cae3127fffd461287f10f268885a26ef64f0d545
Losrik <ly_leyu@outlook.com>
2023-07-19 14:24:18 +0800

    了解MyBatis中逆向工程的构建，以及数据库分页插件

---
+++ LearnSSM/mybatis_cache/src/main/java/com/atguigu/mybatis/Utils/SqlSessionUtil.java
@@ -0,0 +1,30 @@
+package com.atguigu.mybatis.Utils;
+
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SqlSessionUtil {
+    public static SqlSession getSqlSession(){
+        SqlSession sqlSession;
+        try {
+            //获取核心配置流
+            InputStream configStream = Resources.getResourceAsStream("mybatis-config.xml");
+            //获取SqlSessionFactoryBuilder
+            SqlSessionFactoryBuilder sqlSessionFactoryBuilder = new SqlSessionFactoryBuilder();
+            //获取SqlSessionFactory
+            SqlSessionFactory sqlSessionFactory = sqlSessionFactoryBuilder.build(configStream);
+            //获取SqlSession对象,输出参数true，数据库会自动提交事务
+            sqlSession = sqlSessionFactory.openSession(true);
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+        return sqlSession;
+    }
+
+}
