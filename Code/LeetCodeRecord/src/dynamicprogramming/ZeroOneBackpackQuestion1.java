d945df304baaf8cd3b482868bf9b91b03182f9d5
Losrik <ly_leyu@outlook.com>
2023-11-20 09:40:42 +0800

    .

--- Code/LeetCodeRecord/src/dynamicprogramming/ZeroOneBackpackQuestion1.java
+++ Code/LeetCodeRecord/src/dynamicprogramming/ZeroOneBackpackQuestion1.java
@@ -3,7 +3,7 @@ package dynamicprogramming;
 import java.util.Arrays;
 import java.util.Scanner;
 
-public class ZeroOneQuestion {
+public class ZeroOneBackpackQuestion1 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         while (sc.hasNext()) {
